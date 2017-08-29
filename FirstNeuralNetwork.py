f423609cb21223686a65ca6a85d1fff4c6e99930
Ilton Silveira <iltonsilveira91@gmail.com>
2017-08-29 09:12:42 -0300

    initial project, nearest math

---
+++ FirstNeuralNetwork.py
@@ -0,0 +1,205 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data_path = 'bike-sharing-dataset/hour.csv'
+rides = pd.read_csv(data_path)
+
+dummy_fields = ['season', 'weathersit', 'mnth', 'hr', 'weekday']
+for each in dummy_fields:
+    dummies = pd.get_dummies(rides[each], prefix=each, drop_first=False)
+    rides = pd.concat([rides, dummies], axis=1)
+
+fields_to_drop = ['instant', 'dteday', 'season', 'weathersit',
+                  'weekday', 'atemp', 'mnth', 'workingday', 'hr']
+data = rides.drop(fields_to_drop, axis=1)
+
+quant_features = ['casual', 'registered', 'cnt', 'temp', 'hum', 'windspeed']
+# Store scaling in a dictionary so we can convert back later
+scaled_features = {}
+for each in quant_features:
+    mean, std = data[each].mean(), data[each].std()
+    scaled_features[each] = [mean, std]
+    data.loc[:, each] = (data[each] - mean)/std
+
+# Save data for approximately the last 21 days
+test_data = data[-21*24:]
+
+# Now remove the test data from the data set
+data = data[:-21*24]
+
+# Separate the data into features and targets
+target_fields = ['cnt', 'casual', 'registered']
+features, targets = data.drop(target_fields, axis=1), data[target_fields]
+test_features, test_targets = test_data.drop(target_fields, axis=1), test_data[target_fields]
+
+# Hold out the last 60 days or so of the remaining data as a validation set
+train_features, train_targets = features[:-60*24], targets[:-60*24]
+val_features, val_targets = features[-60*24:], targets[-60*24:]
+
+
+class NeuralNetwork(object):
+    def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
+        # Set number of nodes in input, hidden and output layers.
+        self.input_nodes = input_nodes
+        self.hidden_nodes = hidden_nodes
+        self.output_nodes = output_nodes
+
+        # Initialize weights
+        self.weights_input_to_hidden = np.random.normal(0.0, self.hidden_nodes ** -0.5,
+                                                        (self.hidden_nodes, self.input_nodes))
+
+        self.weights_hidden_to_output = np.random.normal(0.0, self.output_nodes ** -0.5,
+                                                         (self.output_nodes, self.hidden_nodes))
+        self.lr = learning_rate
+
+        #### TODO: Set self.activation_function to your implemented sigmoid function ####
+        #
+        # Note: in Python, you can define a function with a lambda expression,
+        # as shown below.
+        self.activation_function = lambda x: 1 / (1 + np.exp(-x))  # Replace 0 with your sigmoid calculation.
+        self.activation_function_prime = lambda x: self.activation_function(x) * (1 - self.activation_function(x))
+
+        ### If the lambda code above is not something you're familiar with,
+        # You can uncomment out the following three lines and put your
+        # implementation there instead.
+        #
+        # def sigmoid(x):
+        #    return 0  # Replace 0 with your sigmoid calculation here
+        # self.activation_function = sigmoid
+
+    def train(self, inputs_list, targets_list):
+        # Convert inputs list to 2d array
+        inputs = np.array(inputs_list, ndmin=2).T
+        targets = np.array(targets_list, ndmin=2).T
+
+        #### Implement the forward pass here ####
+        ### Forward pass ###
+        # TODO: Hidden layer - Replace these values with your calculations.
+        hidden_inputs = np.dot(self.weights_input_to_hidden, inputs)  # signals into hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs)  # signals from hidden layer
+
+        # TODO: Output layer - Replace these values with your calculations.
+        final_inputs = np.dot(hidden_outputs, self.weights_hidden_to_output)  # signals into final output layer
+        final_outputs = self.activation_function(final_inputs)  # signals from final output layer
+
+        #### Implement the backward pass here ####
+        ### Backward pass ###
+
+        # TODO: Output error - Replace this value with your calculations.
+        output_errors = targets - final_outputs  # (y - y^)Output layer error is the difference between desired target and actual output
+        # error_term: y-y^(chapel)*ƒ’(h), ƒ’(h)=1, so error = error_term
+
+        # TODO: Backpropagated error - Replace these values with your calculations.
+        hidden_errors = np.dot(self.weights_hidden_to_output, output_errors)  # errors propagated to the hidden layer
+        hidden_grad = self.activation_function_prime(hidden_errors)  # hidden layer gradients
+
+        # TODO: Update the weights - Replace these values with your calculations.
+        self.weights_hidden_to_output += self.lr * hidden_grad * hidden_outputs.T # update hidden-to-output weights with gradient descent step
+        self.weights_input_to_hidden += 0  # (wi = wi + n(learning-rate)*error-term*xi(input i)) update input-to-hidden weights with gradient descent step
+
+    def run(self, inputs_list):
+        # Run a forward pass through the network
+        inputs = np.array(inputs_list, ndmin=2).T
+
+        #### Implement the forward pass here ####
+        # TODO: Hidden layer - replace these values with the appropriate calculations.
+        hidden_inputs = None  # signals into hidden layer
+        hidden_outputs = None  # signals from hidden layer
+
+        # TODO: Output layer - Replace these values with the appropriate calculations.
+        final_inputs = None  # signals into final output layer
+        final_outputs = np.zeros((1, len(inputs_list)))  # signals from final output layer
+
+        return final_outputs
+
+def MSE(y, Y):
+    return np.mean((y-Y)**2)
+
+
+import sys
+
+### Set the hyperparameters here ###
+epochs = 100
+learning_rate = 0.1
+hidden_nodes = 2
+output_nodes = 1
+
+N_i = train_features.shape[1]
+network = NeuralNetwork(N_i, hidden_nodes, output_nodes, learning_rate)
+
+losses = {'train': [], 'validation': []}
+for e in range(epochs):
+    # Go through a random batch of 128 records from the training data set
+    batch = np.random.choice(train_features.index, size=128)
+    for record, target in zip(train_features.loc[batch].values,
+                              train_targets.loc[batch]['cnt']):
+        network.train(record, target)
+
+    # Printing out the training progress
+    train_loss = MSE(network.run(train_features), train_targets['cnt'].values)
+    val_loss = MSE(network.run(val_features), val_targets['cnt'].values)
+    sys.stdout.write("\rProgress: " + str(100 * e / float(epochs))[:4] \
+                     + "% ... Training loss: " + str(train_loss)[:5] \
+                     + " ... Validation loss: " + str(val_loss)[:5])
+
+    losses['train'].append(train_loss)
+    losses['validation'].append(val_loss)
+
+import unittest
+
+inputs = [0.5, -0.2, 0.1]
+targets = [0.4]
+test_w_i_h = np.array([[0.1, 0.4, -0.3],
+                       [-0.2, 0.5, 0.2]])
+test_w_h_o = np.array([[0.3, -0.1]])
+
+
+class TestMethods(unittest.TestCase):
+    ##########
+    # Unit tests for data loading
+    ##########
+
+    def test_data_path(self):
+        # Test that file path to dataset has been unaltered
+        self.assertTrue(data_path.lower() == 'bike-sharing-dataset/hour.csv')
+
+    def test_data_loaded(self):
+        # Test that data frame loaded
+        self.assertTrue(isinstance(rides, pd.DataFrame))
+
+    ##########
+    # Unit tests for network functionality
+    ##########
+
+    def test_activation(self):
+        network = NeuralNetwork(3, 2, 1, 0.5)
+        # Test that the activation function is a sigmoid
+        self.assertTrue(np.all(network.activation_function(0.5) == 1 / (1 + np.exp(-0.5))))
+
+    def test_train(self):
+        # Test that weights are updated correctly on training
+        network = NeuralNetwork(3, 2, 1, 0.5)
+        network.weights_input_to_hidden = test_w_i_h.copy()
+        network.weights_hidden_to_output = test_w_h_o.copy()
+
+        network.train(inputs, targets)
+        print('w_h_o: ', network.weights_hidden_to_output)
+        self.assertTrue(np.allclose(network.weights_hidden_to_output,
+                                    np.array([[0.37275328, -0.03172939]])))
+        print('w_i_h: ', network.weights_input_to_hidden)
+        self.assertTrue(np.allclose(network.weights_input_to_hidden,
+                                    np.array([[0.10562014, 0.39775194, -0.29887597],
+                                              [-0.20185996, 0.50074398, 0.19962801]])))
+
+    def test_run(self):
+        # Test correctness of run method
+        network = NeuralNetwork(3, 2, 1, 0.5)
+        network.weights_input_to_hidden = test_w_i_h.copy()
+        network.weights_hidden_to_output = test_w_h_o.copy()
+
+        self.assertTrue(np.allclose(network.run(inputs), 0.09998924))
+
+
+suite = unittest.TestLoader().loadTestsFromModule(TestMethods())
+unittest.TextTestRunner().run(suite)
---
+++ FirstNeuralNetworkTest.py
@@ -0,0 +1,57 @@
+import unittest
+
+inputs = [0.5, -0.2, 0.1]
+targets = [0.4]
+test_w_i_h = np.array([[0.1, 0.4, -0.3],
+                       [-0.2, 0.5, 0.2]])
+test_w_h_o = np.array([[0.3, -0.1]])
+
+
+class TestMethods(unittest.TestCase):
+    ##########
+    # Unit tests for data loading
+    ##########
+
+    def test_data_path(self):
+        # Test that file path to dataset has been unaltered
+        self.assertTrue(data_path.lower() == 'bike-sharing-dataset/hour.csv')
+
+    def test_data_loaded(self):
+        # Test that data frame loaded
+        self.assertTrue(isinstance(rides, pd.DataFrame))
+
+    ##########
+    # Unit tests for network functionality
+    ##########
+
+    def test_activation(self):
+        network = NeuralNetwork(3, 2, 1, 0.5)
+        # Test that the activation function is a sigmoid
+        self.assertTrue(np.all(network.activation_function(0.5) == 1 / (1 + np.exp(-0.5))))
+
+    def test_train(self):
+        # Test that weights are updated correctly on training
+        network = NeuralNetwork(3, 2, 1, 0.5)
+        network.weights_input_to_hidden = test_w_i_h.copy()
+        network.weights_hidden_to_output = test_w_h_o.copy()
+
+        network.train(inputs, targets)
+        print('w_h_o: ', network.weights_hidden_to_output)
+        self.assertTrue(np.allclose(network.weights_hidden_to_output,
+                                    np.array([[0.37275328, -0.03172939]])))
+        print('w_i_h: ', network.weights_input_to_hidden)
+        self.assertTrue(np.allclose(network.weights_input_to_hidden,
+                                    np.array([[0.10562014, 0.39775194, -0.29887597],
+                                              [-0.20185996, 0.50074398, 0.19962801]])))
+
+    def test_run(self):
+        # Test correctness of run method
+        network = NeuralNetwork(3, 2, 1, 0.5)
+        network.weights_input_to_hidden = test_w_i_h.copy()
+        network.weights_hidden_to_output = test_w_h_o.copy()
+
+        self.assertTrue(np.allclose(network.run(inputs), 0.09998924))
+
+
+suite = unittest.TestLoader().loadTestsFromModule(TestMethods())
+unittest.TextTestRunner().run(suite)
